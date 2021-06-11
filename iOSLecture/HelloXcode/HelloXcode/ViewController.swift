2eed97d13948c372a544b2852781c90241104a0f
Seongbeom Hong <ponoponong@gmail.com>
2021-06-11 23:08:41 +0900

    오늘은 간단하게 iOS 맛보기! iOS勉強スタート！！

---
+++ iOSLecture/HelloXcode/HelloXcode/ViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ViewController.swift
+//  HelloXcode
+//
+//  Created by 홍성범 on 2021/06/11.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        
+        let a = 10
+        print(a)
+    }
+
+
+}
+
