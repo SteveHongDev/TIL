faa733fcb81c09c2e7ce358124ee5c2202d42ef2
Seongbeom Hong <ponoponong@gmail.com>
2021-06-12 22:10:40 +0900

    시험공부 떄문에 많이 못하는 중

---
+++ iOSLecture/OutletAndAction/OutletAndAction/ViewController.swift
@@ -0,0 +1,41 @@
+//
+//  ViewController.swift
+//  OutletAndAction
+//
+//  Created by 홍성범 on 2021/06/12.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    
+    @IBOutlet weak var lbl: UILabel!
+    
+    
+    
+    @IBAction func test(_ sender: Any) {
+        
+    }
+    
+    
+    
+    
+    @IBOutlet weak var btn: UIButton!
+    
+    
+    
+    
+    
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        
+        btn.backgroundColor = UIColor.red
+    }
+
+
+}
+
